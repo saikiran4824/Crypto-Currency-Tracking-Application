8a35917987ba26741020faabe42259df238dbd91
SAI KIRAN <saikiran4824@gmail.com>
2024-10-26 21:04:15 +0530

    responsive

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -5,9 +5,14 @@ import TableComponent from "../components/TableComponent";
 
 const Crypto = () => {
   return (
-    <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+    <section className="w-full max-w-6xl h-full flex flex-col mt-8 mb-16 px-4 md:px-8 lg:px-16">
+      {/* Filters component */}
       <Filters />
+      
+      {/* Table component */}
       <TableComponent />
+
+      {/* Outlet component for nested routes */}
       <Outlet />
     </section>
   );
